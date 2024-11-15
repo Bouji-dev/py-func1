95f54e55d2d4cf46c5092c660b795f73760d589e
BoujiDev <dev2024loper@gmail.com>
2024-11-15 13:12:08 +0330

    write csv file

--- write.csv.py
+++ write.csv.py
@@ -1,12 +1,14 @@
 import csv
+import pandas as pd
+
 headerfile = ["state", "population", "city"]
 data = [["Tehran", 14, "Tehran"] , ["Fars", 7, "Shiraz"], ["Markazi",3,"Arak"]]
 
-with open("data05.csv",mode="w",newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(headerfile)
-    writer.writerows(data)
+# with open("data05.csv",mode="w",newline="") as f:
+#     writer = csv.writer(f)
+#     writer.writerow(headerfile)
+#     writer.writerows(data)
 
 
-
-
+data05 = pd.read_csv("data05.csv")
+print(data05)
